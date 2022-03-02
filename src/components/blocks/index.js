02b933e8b62149dd045f87e03d78dc5412cdb849
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-02 18:30:50 +0100

    blog d

--- src/components/blocks/index.js
+++ src/components/blocks/index.js
@@ -1,6 +1,7 @@
 import BlogA from './blog/BlogA';
 import BlogB from './blog/BlogB';
 import BlogC from './blog/BlogC';
+import BlogD from './blog/BlogD';
 
 
 export default function getBlock(theme) {
@@ -9,6 +10,7 @@ export default function getBlock(theme) {
       BlogA: <BlogA theme={theme} />,
       BlogB: <BlogB theme={theme} />,
       BlogC: <BlogC theme={theme} />,
+      BlogD: <BlogD theme={theme} />,
     },
   }
 }
