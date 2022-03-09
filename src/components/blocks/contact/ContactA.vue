b2e81176a6ccb537079dc03b907dd90a5970bff6
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-09 12:48:50 +0100

    focus not working

--- src/components/blocks/contact/ContactA.vue
+++ src/components/blocks/contact/ContactA.vue
@@ -1,6 +1,6 @@
 <template>
-    <section class="text-gray-400 bg-gray-900 body-font relative">
-      <div class="absolute inset-0 bg-gray-900">
+    <section class="text-gray-600 dark:text-gray-400 dark:bg-gray-900 body-font relative">
+      <div class="absolute inset-0 bg-gray-300 dark:bg-gray-900">
         <iframe
           title="map"
           width="100%"
@@ -14,8 +14,8 @@
         />
       </div>
       <div class="container px-5 py-24 mx-auto flex">
-        <div class="lg:w-1/3 md:w-1/2 bg-gray-900 shadow-md rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
-          <h2 class="text-white text-lg mb-1 font-medium title-font">
+        <div class="lg:w-1/3 md:w-1/2 bg-white dark:bg-gray-900 shadow-md rounded-lg p-8 flex flex-col md:ml-auto w-full mt-10 md:mt-0 relative z-10">
+          <h2 class="text-gray-900 dark:text-white text-lg mb-1 font-medium title-font">
             Feedback
           </h2>
           <p class="leading-relaxed mb-5">
@@ -32,22 +32,22 @@
               type="email"
               id="email"
               name="email"
-              class="`w-full bg-gray-800 rounded border border-gray-700 focus:ring-2 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`"
-              :class="` focus:border-${theme}-500  focus:ring-${theme}-900`"
+              class="w-full bg-white dark:bg-gray-800 rounded border border-gray-300 dark:border-gray-700 focus:ring-2 text-base outline-none text-gray-100 dark:text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out"
+              :class="` focus:${borderColor500}  focus:${focusRingColor900} dark:focus:${focusRingColor200}`"
             />
           </div>
           <div class="relative mb-4">
             <label
               htmlFor="message"
-              class="leading-7 text-sm text-gray-400"
+              class="leading-7 text-sm text-gray-600 dark:text-gray-400"
             >
               Message
             </label>
             <textarea
               id="message"
               name="message"
-              class="`w-full bg-gray-800 rounded border border-gray-700 focus:ring-2 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out`"
-              :class="` focus:border-${theme}-500  focus:ring-${theme}-900`"
+              class="w-full bg-white dark:bg-gray-800 rounded border border-gray-300 dark:border-gray-700 focus:ring-2 h-32 text-base outline-none text-gray-700 dark:text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out"
+              :class="` focus:${borderColor500}  ${focusRingColor900}`"
             ></textarea>
           </div>
           <button class="`text-white border-0 py-2 px-6 focus:outline-none rounded text-lg`"
@@ -55,7 +55,7 @@
           >
             Button
           </button>
-          <p class="text-xs text-gray-400 text-opacity-90 mt-3">
+          <p class="text-xs text-gray-50 dark:text-gray-400 dark:text-opacity-90 mt-3">
             Chicharrones blog helvetica normcore iceland tousled brook viral
             artisan.
           </p>
@@ -67,8 +67,8 @@
 import { useTheme } from "@/compossable/theme";
 export default {
   setup() {
-    const { textColor, textBgColor, theme, bgColor600, bgColor500 } = useTheme()
-    return { textColor, textBgColor, theme, bgColor600, bgColor500 }
+    const { textColor, textBgColor, theme, bgColor600, bgColor500, borderColor500, focusRingColor500, focusRingColor200, focusRingColor900 } = useTheme()
+    return { textColor, textBgColor, theme, bgColor600, bgColor500, borderColor500, focusRingColor500, focusRingColor200, focusRingColor900 }
   }
 }
 </script>
