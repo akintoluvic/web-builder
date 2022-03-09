c9dbe59584ab1788ecd11304f307542d6694dee0
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-09 13:00:04 +0100

    fix scale

--- src/views/BuilderView.vue
+++ src/views/BuilderView.vue
@@ -37,8 +37,8 @@
 
       <!-- design preview/code -->
       <div class="flex-auto h-screen overflow-y-scroll justify-center">
-        <main class="px-8 my-12 bg-white min-h-screen">
-          <div v-for="(currentIcon, index) in selectedIcons" :key="index" class="scale-90">
+        <main class="px-8 my-12 min-h-screen">
+          <div v-for="(currentIcon, index) in selectedIcons" :key="index">
             <component :is="blocksList[currentIcon[1]][currentIcon[0]]"  />
           </div>
         </main>
