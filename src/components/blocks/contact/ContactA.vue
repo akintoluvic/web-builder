2813f59e2137a9773acf8a89201d6ac921fa9a9b
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-09 12:19:36 +0100

    contact a dark

--- src/components/blocks/contact/ContactA.vue
+++ src/components/blocks/contact/ContactA.vue
@@ -32,7 +32,8 @@
               type="email"
               id="email"
               name="email"
-              class="`w-full bg-gray-800 rounded border border-gray-700 focus:border-$"props.theme"-500 focus:ring-2 focus:ring-$"props.theme"-900 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`"
+              class="`w-full bg-gray-800 rounded border border-gray-700 focus:ring-2 text-base outline-none text-gray-100 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out`"
+              :class="` focus:border-${theme}-500  focus:ring-${theme}-900`"
             />
           </div>
           <div class="relative mb-4">
@@ -45,7 +46,8 @@
             <textarea
               id="message"
               name="message"
-              class="`w-full bg-gray-800 rounded border border-gray-700 focus:border-$"props.theme"-500 focus:ring-2 focus:ring-$"props.theme"-900 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out`"
+              class="`w-full bg-gray-800 rounded border border-gray-700 focus:ring-2 h-32 text-base outline-none text-gray-100 py-1 px-3 resize-none leading-6 transition-colors duration-200 ease-in-out`"
+              :class="` focus:border-${theme}-500  focus:ring-${theme}-900`"
             ></textarea>
           </div>
           <button class="`text-white border-0 py-2 px-6 focus:outline-none rounded text-lg`"
