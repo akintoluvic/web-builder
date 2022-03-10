6242e9f64be9fd9d72f85b5018ef3a14c277c590
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-10 13:31:51 +0100

    use component update

---
+++ src/compossable/components.js
@@ -0,0 +1,34 @@
+import { ref, onMounted } from 'vue'
+import getIcons from "@/components/icons"
+import getBlocks from "@/components/blocks"
+
+const iconList = ref(null)
+const blocksList = ref(null)
+const blockListArr = ref([])
+const selectedIcons = ref([])
+
+blocksList.value = getBlocks()
+iconList.value = getIcons()
+
+export function useComponents() {
+
+    const addComponent = val => {
+      selectedIcons.value = [...selectedIcons, val]
+    }
+
+    onMounted(() => {
+        Object.entries(iconList).forEach(([type, icons]) => {
+          Object.keys(icons).map((name) =>
+            blockListArr.value.push(`${name},${type}`)
+          );
+        })
+      })
+
+    return {
+      iconList,
+      blocksList,
+      blockListArr,
+      selectedIcons,
+      addComponent,
+    }
+}
