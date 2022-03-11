d50da76cc24786627a129b6d940136c62ba28ad1
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-11 09:51:23 +0100

    dark bg

--- src/components/builder/CodeOrPagePreview.vue
+++ src/components/builder/CodeOrPagePreview.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="flex-auto bg-white h-screen overflow-y-scroll justify-center">
+  <div class="flex-auto bg-white dark:bg-gray-900 h-screen overflow-y-scroll justify-center">
     <main class="px-8 my-12 min-h-screen">
         <div v-for="(currentIcon, index) in selectedIcons" :key="index">
         <component :is="blocksList[currentIcon[1]][currentIcon[0]]"  />
