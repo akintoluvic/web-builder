b69fc92eb84fd75fb8485752d6f50faa72862f06
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-14 09:59:35 +0100

    improve ui

--- src/components/builder/LeftSideBar.vue
+++ src/components/builder/LeftSideBar.vue
@@ -11,7 +11,7 @@
         <svg @click="removeSelectComponent(index)" class="w-3 h-3 cursor-pointer hover:bg-slate-300" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clip-rule="evenodd"></path></svg>
       </div>
     </div>
-    <h3 class="px-4 text-sm border-t border-gray-400">Add new section</h3>
+    <h3 class="px-4 pt-3 text-sm text-slate-700 border-t border-gray-300">Add section</h3>
     <div
       v-for="(icons, type) in iconList"
       :key="type"
