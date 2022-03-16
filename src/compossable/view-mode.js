802e5286bb6aece47b8bf0e6dec2ea2031d839bc
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-16 16:01:13 +0100

    fix use code view

---
+++ src/compossable/view-mode.js
@@ -0,0 +1,24 @@
+import { ref, onMounted } from "vue"
+const viewType = ref('desktop')
+const codeView = ref(false)
+
+export function useViewOrCode() {
+  onMounted(() => {
+    // On page load or when changing themes, best to add inline in `head` to avoid FOUC
+  })
+
+  const setViewType = value => {
+    viewType.value = value
+  }
+
+  const toggleCodeView = () => {
+    codeView.value = !codeView.value
+  }
+
+  return {
+    setViewType,
+    toggleCodeView,
+    codeView,
+    viewType,
+  }
+}
