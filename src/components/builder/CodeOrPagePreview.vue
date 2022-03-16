b94214185a7d6bb4616bae0510c4e5a3b1e4d726
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-16 13:46:56 +0100

    rough preview in iframe

--- src/components/builder/CodeOrPagePreview.vue
+++ src/components/builder/CodeOrPagePreview.vue
@@ -1,7 +1,20 @@
 <template>
   <div class="flex-auto bg-white dark:bg-gray-900 h-screen overflow-y-scroll justify-center" @click="showCode">
-
-    <main class="px-8 my-12 min-h-screen" ref="codeBlock">
+    <div  class="w-full h-full text-gray-900">
+      <iframe 
+        class="mt-32 w-2/3 h-52 bg-blue-400"
+        title="Page preview"
+        :onload="logShit"
+        loading="lazy"
+        :name="lalac"
+        :srcdoc="lilas"
+      >
+        
+      </iframe>
+      <component :is="blocksList[selectedIcons[0][1]][selectedIcons[0][0]]"  />
+      <div v-html="codeBlock?.value?.innerHTML"></div>
+    </div> 
+    <main class="hidden px-8 my-12 min-h-screen" ref="codeBlock">
       <div v-for="(currentIcon, index) in selectedIcons" :key="index">
         <component :is="blocksList[currentIcon[1]][currentIcon[0]]"  />
       </div>
@@ -10,23 +23,53 @@
 </template>
 <script>
 import { useComponents } from "@/compossable/components"
-import { ref } from "vue";
+import { useDarkMode } from "@/compossable/dark-mode"
+import { ref, onMounted, computed } from "vue";
 
 export default {
   name: 'CodeOrPagePreview',
+  data() {
+    return {
+      asee: "Weret",
+    }
+  },
   setup() {
     const { 
       blocksList,
       selectedIcons,
     } = useComponents()
+    const { darkMode } = useDarkMode()
 
-    const codeBlock = ref(null)
+    const codeBlock = ref('')
+    const codeForPreview = ref('<span>Emile</span>')
     
     const showCode = () => {
-      console.log(codeBlock.value.innerHTML)
+      // codeForPreview.value = codeBlock.value.innerHTML
+      console.log(typeof codeForPreview.value)
+      console.log(codeForPreview.value)
     }
+
+    onMounted(() => {
+        codeBlock.value = document.querySelector('main')
+      })
+
+    const lilas = computed(() => {
+        return `<span>${codeBlock.value.innerHTML}</span>`
+      })
     
-    return { selectedIcons, blocksList, showCode, codeBlock }
+    const logShit = () => {
+      console.log('Hmmm', showCode())
+    }
+    return {
+      selectedIcons,
+      blocksList,
+      showCode,
+      codeForPreview,
+      codeBlock,
+      darkMode,
+      logShit,
+      lilas,
+    }
   },
 }
 </script>
