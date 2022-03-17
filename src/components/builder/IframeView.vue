8a0852d1d8d1e3861fae0d222900f6f3cb6ff486
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-17 14:33:20 +0100

    init iFrame view

---
+++ src/components/builder/IframeView.vue
@@ -0,0 +1,25 @@
+<template>
+  <iframe 
+        ref="iframe"
+        class="h-full bg-white dark:bg-gray-900 mx-auto border-2 border-slate-200"
+        :class="[viewWidth, ]"
+        title="Page components preview"
+        :onload="logShit"
+        loading="lazy"
+        name="Page components preview"
+      />
+</template>
+<script>
+import { useViewOrCode } from "@/compossable/view-mode"
+
+export default {
+  name: 'IframeView',
+  setup() {
+    const { viewWidth, codeView, } = useViewOrCode()
+    return {
+      viewWidth,
+      codeView,
+    }
+  }
+}
+</script>
