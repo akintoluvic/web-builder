1d22dbbb3751e2fc5d48e62d9c553b73fd2452c0
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-17 15:43:32 +0100

    IframeView now part fix

--- src/components/builder/IframeView.vue
+++ src/components/builder/IframeView.vue
@@ -4,21 +4,53 @@
         class="h-full bg-white dark:bg-gray-900 mx-auto border-2 border-slate-200"
         :class="[viewWidth, ]"
         title="Page components preview"
-        :onload="logShit"
         loading="lazy"
         name="Page components preview"
       />
 </template>
 <script>
 import { useViewOrCode } from "@/compossable/view-mode"
+import { toRefs, onMounted, ref,  } from "vue"
 
 export default {
   name: 'IframeView',
-  setup() {
+  props: {
+    code: String
+  },
+  setup(props) {
+    const { code } = toRefs(props)
     const { viewWidth, codeView, } = useViewOrCode()
+    const iframe = ref('')
+
+    // computed property that auto-updates when the prop changes
+    // const normalizedSize = computed(() => props.size.trim().toLowerCase())
+    
+    const displayContent = () => {
+        iframe.value.contentWindow.document.write(code.value)
+    }
+
+    console.log(code, 'codeForView')
+
+    onMounted(() => {
+      displayContent()
+    })
+
+    const logShit = () => {
+      console.log('Hmmm, what iFrame doing here?')
+    }
+
+
+    // watch(codeForView, () => {
+    //   displayContent(codeForView)
+        
+    //   console.log('iframe updated', codeForView.value)
+    // })
+
     return {
       viewWidth,
       codeView,
+      iframe,
+      logShit,
     }
   }
 }
