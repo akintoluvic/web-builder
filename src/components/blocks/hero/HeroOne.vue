40d4b7817c3f288f5ca7d217e07b79cff776d29b
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-21 14:26:34 +0100

    header dark

--- src/components/blocks/hero/HeroOne.vue
+++ src/components/blocks/hero/HeroOne.vue
@@ -1,8 +1,8 @@
 <template>
-  <section class="text-gray-600 body-font">
+  <section class="text-gray-600 dark:text-gray-400 dark:bg-gray-900 body-font">
       <div class="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
         <div class="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
-          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900">
+          <h1 class="title-font sm:text-4xl text-3xl mb-4 font-medium text-gray-900 dark:text-white">
             Before they sold out<br class="hidden lg:inline-block" />
             readymade gluten
           </h1>
