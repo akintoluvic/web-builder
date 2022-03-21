4df0fe4687c888b9154a41c017b11752f1f5c141
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-21 12:52:51 +0100

    hover header button

--- src/components/blocks/header/HeaderA.vue
+++ src/components/blocks/header/HeaderA.vue
@@ -34,7 +34,7 @@
               Fourth Link
             </a>
           </nav>
-          <button class="inline-flex items-center bg-gray-800 dark:bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          <button class="inline-flex items-center bg-gray-800 dark:bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 dark:hover:bg-gray-200 rounded text-base mt-4 md:mt-0">
             Button
             <svg
               fill="none"
