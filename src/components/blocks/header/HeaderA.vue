ed3c578a2bdd06057c8e42afe479c07c3c8cd940
Vick Greenfields <32616421+akintoluvic@users.noreply.github.com>
2022-03-21 12:50:24 +0100

    fix header a

--- src/components/blocks/header/HeaderA.vue
+++ src/components/blocks/header/HeaderA.vue
@@ -3,7 +3,7 @@
         <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
           <a
             href
-            class="flex title-font font-medium items-center text-white mb-4 md:mb-0"
+            class="flex title-font font-medium items-center text-gray-900 dark:text-white mb-4 md:mb-0"
           >
             <svg
               xmlns="http://www.w3.org/2000/svg"
@@ -21,20 +21,20 @@
             <span class="ml-3 text-xl">Tailblocks</span>
           </a>
           <nav class="md:ml-auto flex flex-wrap items-center text-base justify-center">
-            <a href class="mr-5 hover:text-white">
+            <a href class="mr-5 hover:text-gray-900 dark:hover:text-white">
               First Link
             </a>
-            <a href class="mr-5 hover:text-white">
+            <a href class="mr-5 hover:text-gray-900 dark:hover:text-white">
               Second Link
             </a>
-            <a href class="mr-5 hover:text-white">
+            <a href class="mr-5 hover:text-gray-900 dark:hover:text-white">
               Third Link
             </a>
-            <a href class="mr-5 hover:text-white">
+            <a href class="mr-5 hover:text-gray-900 dark:hover:text-white">
               Fourth Link
             </a>
           </nav>
-          <button class="inline-flex items-center bg-gray-800 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
+          <button class="inline-flex items-center bg-gray-800 dark:bg-gray-100 border-0 py-1 px-3 focus:outline-none hover:bg-gray-700 rounded text-base mt-4 md:mt-0">
             Button
             <svg
               fill="none"
